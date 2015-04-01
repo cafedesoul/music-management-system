20b1a2e71a4ba979fa1cc6f5b236a34366a26942
drainpip <drainpip@gmail.com>
2015-03-31 21:44:03 -0700

    Add slugs to fixture

--- app/server/fixtures.js
+++ app/server/fixtures.js
@@ -1,16 +1,19 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
+    slug: 'introducing-telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
 
   Posts.insert({
     title: 'Meteor',
+    slug: 'meteor',
     url: 'http://meteor.com'
   });
 
   Posts.insert({
     title: 'The Meteor Book',
+    slug: 'the-meteor-book',
     url: 'http://themeteorbook.com'
   });
 }
